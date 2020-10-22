3aef69c9c38538fbe6f2496bb3c62469712c0fea
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2020-10-21 18:06:44 -0700

    Update Main.java

--- ProgramasJAVA/src/Faculdade/S3U2/Main.java
+++ ProgramasJAVA/src/Faculdade/S3U2/Main.java
@@ -6,23 +6,24 @@ import javax.sound.midi.MidiUnavailableException;
 
 /**
  * @author Wellington Santos - WSantos79
- * Cï¿½digo 3.21
+ * Código 3.21
  */
 public class Main {
 
     public static void main(String[] args) throws MidiUnavailableException {
         System.out.println("Tocando a melodia no piano velocidade normal");
+        Melodia melodiaNormal = new Melodia(1.00);
         Piano piano = new Piano();
         piano.setVolume(75);
         piano.tocar(melodiaNormal);
         
-        System.out.println("Tocando a melodia no violancelo velocidade rï¿½pida");
+        System.out.println("Tocando a melodia no violancelo velocidade rápida");
         Melodia melodiaRapida = new Melodia(1.25);
         Violoncelo violoncelo = new Violoncelo();
         violoncelo.setVolume(100);
         violoncelo.tocar(melodiaRapida);
         
-        System.out.println("Tocando a melodia no violï¿½o velocidade lenta");
+        System.out.println("Tocando a melodia no violão velocidade lenta");
         Melodia melodiaLenta = new Melodia(0.75);
         Violao violao = new Violao();
         violao.setVolume(50);
