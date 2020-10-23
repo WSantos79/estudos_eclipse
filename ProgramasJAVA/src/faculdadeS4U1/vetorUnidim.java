81f66737b411b8e5765fa246dc5b436ef2b3b729
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2020-10-23 14:22:18 -0700

    Commit

---
+++ ProgramasJAVA/src/faculdadeS4U1/vetorUnidim.java
@@ -0,0 +1,21 @@
+package faculdadeS4U1;
+
+public class vetorUnidim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int vet[] = new int[11];
+		
+		for (int i = 0; i < vet.length; i++) {
+			
+			vet[i] = (i+1) * 2;
+			System.out.println("Array(vetor)[" + i + "]: " + vet[i]);
+		}
+				
+		
+
+	}
+
+}
