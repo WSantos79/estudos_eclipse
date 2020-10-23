48c2012991c3bcbb0b9d5b8e20c5d4e846d5821c
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2020-10-23 15:28:42 -0700

    Commit

--- ProgramasJAVA/src/faculdadeS4U1/tipoStrings.java
+++ ProgramasJAVA/src/faculdadeS4U1/tipoStrings.java
@@ -23,10 +23,26 @@ public class tipoStrings {
 		str2 = ("cuuuuu");
 		
 		System.out.println(str2);
+		System.out.println();
+		
+		
+		tipoStrings tipoStrings = new tipoStrings();
+		System.out.println();
+		tipoStrings.criacaoLiterais();
+		System.out.println();
+		tipoStrings.sequenciaDeCaracteres();
+		System.out.println();
+		tipoStrings.metodosStrings();
+		System.out.println();
+		tipoStrings.concatenaString();
+		System.out.println();
+		//tipoStrings.superConcatenacaoStrings();
+		System.out.println();
+		tipoStrings.comparacaoStrings();
 		
 	}
  
-}
+
 
 
 /**
@@ -54,4 +70,84 @@ atribuição direta de valores literais
  *   A vantagem da string pool sobre a heap é que ela economiza gastos com memória. 
  *
  */
+
+public void criacaoLiterais(){
+    String str1 = new String("Orientação a objetos");
+    String str2 = "Orientação a objetos";
+    
+    StringBuilder str3 = new StringBuilder("Orientação a objetos");
+    //StringBuilder str4 = "Orientação a objetos";//não é aceito
+    
+    StringBuffer str5 = new StringBuffer("Orientação a objetos");
+    //StringBuffer str6 = "Orientação a objetos";//não é aceito
+    System.out.println("Criacao de literais: ");
+    System.out.println("str1: " + str1);
+    System.out.println("str2: " + str2);
+    System.out.println("str3: " + str3);
+    //System.out.println("str4: " + str4);
+    System.out.println("str5: " + str5);
+    //System.out.println("str6: " + str6);
+}
+
+public void sequenciaDeCaracteres(){
+	System.out.println("Sequencia de Caracteres: ");
+    char seqChar[] = {'l', 'i', 'v', 'r', 'o'};
+    System.out.println(seqChar);
+    for (char elemento : seqChar) {
+        System.out.print(elemento);
+    }
+    System.out.println();
+}
+
+public void metodosStrings(){
+	System.out.println("Metodos Strings: ");
+    String nomeLivro = "Linguagem Orientada a Objetos";
+    System.out.println(nomeLivro.toUpperCase());
+    System.out.println(nomeLivro.toLowerCase());
+    System.out.println(nomeLivro.replace("Linguagem", "Ling."));
+    System.out.println(nomeLivro.length());
+    System.out.println(nomeLivro.endsWith("Objetos"));
+    System.out.println(nomeLivro.startsWith("Ling"));
+    System.out.println(nomeLivro.contains("Orient"));
+    String sp[] = nomeLivro.split(" ");
+    for (String elem : sp) {
+        System.out.println("elem: " + elem);
+    }
+    StringBuffer sBuffer = new StringBuffer("Linguagem");
+    System.out.println("reverse: " + sBuffer);
+    sBuffer = sBuffer.reverse();
+    System.out.println("reverse: " + sBuffer);        
+}
+
+public void concatenaString() {
+	System.out.println("Concatena String: ");
+    String livro = "Linguagem";
+    System.out.println(livro);
+    livro = livro.concat(" Orientada");
+    System.out.println(livro);
+    livro += " a Objetos";
+    System.out.println(livro);
+}
+
+public void superConcatenacaoStrings() {
+	System.out.println("Super Concaternacao String: ");
+    String superString = "";
+    for (int i = 0; i < 100000; i++) {
+        superString += ".";
+    }
+    System.out.println(superString);
+}
+
+public void comparacaoStrings() {
+	System.out.println("Comparacao String: ");
+    String s1 = "teste";
+    String s2 = new String("teste");
+    String s3 = s1;
+    String s4 = new String("teste");
+    System.out.printf("s2: %b\n", s1==s2);
+    System.out.printf("s3: %b\n", s1==s3);
+    System.out.printf("s3: %b\n", s2==s3);
+    System.out.printf("s4: %b\n", s2==s4);
+}
+}
 
