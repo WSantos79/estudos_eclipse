51b9ca7a1d7048a74e7de91143630b7b55e79d5e
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2020-10-24 16:04:53 -0700

    Commit

---
+++ ProgramasJAVA/src/faculdade/S4U2/SP/bd/Consultas.java
@@ -0,0 +1,25 @@
+package faculdade.S4U2.SP.bd;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Jesimar da Silva Arantes
+ * Código 4.14
+ */
+public class Consultas {
+    
+    public void inserirPos(Connection connection, float posX, float posY){
+        try {
+            Statement st = connection.createStatement();
+            String sql = "INSERT INTO `rota` (posx, posy) " + 
+                    "VALUES (" + posX + ", " + posY + ")";
+            st.executeUpdate(sql);
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println("Problema de acesso ao banco de dados");
+            System.out.println(ex);
+        }
+    }
+}
---
+++ ProgramasJAVA/src/faculdade/S4U2/SP/core/Orientacao.java
@@ -0,0 +1,10 @@
+package faculdade.S4U2.SP.core;
+
+/**
+ * Enumeração que modela a orientação do robô.
+ * @author Jesimar S. Arantes
+ * Código 4.14
+ */
+public enum Orientacao {
+    FRENTE, ATRAS, ESQUERDA, DIREITA;
+}
